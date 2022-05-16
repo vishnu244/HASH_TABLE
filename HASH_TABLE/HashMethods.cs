7ffae5a5fff933c8c4da2051947c1b9cd48d0868
vishnu244 <vishnunali244@gmail.com>
2022-05-16 12:18:48 +0530

    [Vishnu]- checking the frequency of a sentence

---
+++ HASH_TABLE/HashMethods.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HASH_TABLE
+{
+    public class HashMethods<K, V>
+    {
+        public int Size { get; set; }
+        public int Count { get; set; }
+        public LinkedList<keyValues<K, V>>[] items;
+        public HashMethods(int size)
+        {
+            this.Size = size;
+            this.items = new LinkedList<keyValues<K, V>>[size];
+
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<keyValues<K, V>> linkedlist = GetLinkedlist(position);
+            keyValues<K, V> item = new keyValues<K, V>() { key = key, Value = value };
+            linkedlist.AddLast(item);
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % Size;
+            return Math.Abs(position);
+        }
+        public V GetValue(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<keyValues<K, V>> linkedlist = GetLinkedlist(position);
+            foreach (keyValues<K, V> item in linkedlist)
+            {
+                if (item.key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public LinkedList<keyValues<K, V>> GetLinkedlist(int position)
+        {
+            LinkedList<keyValues<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<keyValues<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+
+
+        public void Display()
+        {
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                {
+                    foreach (keyValues<K, V> keyvalue in linkedList)
+                    {
+                        Console.WriteLine(keyvalue.key + " --> " + keyvalue.Value);
+                    }
+                }
+            }
+        }
+    }
+
+    public struct keyValues<K, V>
+    {
+        public K key { get; set; }
+        public V Value { get; set; }
+    }
+}
---
+++ HASH_TABLE/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HASH_TABLE;
+
+Console.WriteLine("WELCOME TO HASHTABLE PROBLEM!");
+
+
+while (true)
+{
+    Console.WriteLine("Please choose the option :\n1) Frequency of a sentence");
+    int option = Convert.ToInt16(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            HashMethods<string, int> hash = new HashMethods<string, int>(5);
+            string Words = "To be or not to be";
+            string[] array = Words.Split(' ');
+            LinkedList<string> checkForDuplicationS = new LinkedList<string>();
+            foreach (string element in array)
+            {
+                int count = 0;
+                foreach (string match in array)
+                {
+                    if (element == match)
+                    {
+                        count++;
+                        if (checkForDuplicationS.Contains(element))
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (!checkForDuplicationS.Contains(element))
+                {
+                    checkForDuplicationS.AddLast(element);
+                    hash.Add(element, count);
+                }
+            }
+            hash.Display();
+            break;
+
+        default:
+            break;
+    }
+}
