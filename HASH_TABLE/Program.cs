925a64adefb1be6cd36837b226385a21f44f1ba7
vishnu244 <vishnunali244@gmail.com>
2022-05-16 12:31:10 +0530

    [Vishnu]- checking the frequency of a Paragraph

--- HASH_TABLE/Program.cs
+++ HASH_TABLE/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("WELCOME TO HASHTABLE PROBLEM!");
 
 while (true)
 {
-    Console.WriteLine("Please choose the option :\n1) Frequency of a sentence");
+    Console.WriteLine("Please choose the option :\n1)Frequency of a sentence\n2)Frequency of a Paragraph");
     int option = Convert.ToInt16(Console.ReadLine());
     switch (option)
     {
@@ -38,7 +38,33 @@ while (true)
             hash.Display();
             break;
 
-        default:
+        case 2:
+            HashMethods<string, int> hash2 = new HashMethods<string, int>(5);
+            string Word = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] array2 = Word.Split(' ');
+            LinkedList<string> checkForDuplication2 = new LinkedList<string>();
+            foreach (string element in array2)
+            {
+                int count = 0;
+                foreach (string match in array2)
+                {
+                    if (element == match)
+                    {
+                        count++;
+                        if (checkForDuplication2.Contains(element))
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (!checkForDuplication2.Contains(element))
+                {
+                    checkForDuplication2.AddLast(element);
+                    hash2.Add(element, count);
+                }
+            }
+            hash2.Display();
             break;
+
     }
 }
